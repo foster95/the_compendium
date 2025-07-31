f0d326c08589aee8fd6f1a93dbd96c56be954962
foster95 <alicerosefoster95@gmail.com>
2025-07-31 15:44:06 +0100

    Wire up APIs and confirm access to google sheet using Love Sandwiches walk through

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""Dependencies imports"""
+import gspread
+from google.oauth2.service_account import Credentials 
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+"""Code from Love Sandwiches Walkthrough"""
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD.open('the_compendium')
+
+stored_characters = SHEET.worksheet('Stored Characters')
+
+characters = stored_characters.get_all_values()
+
+for character in characters:
+    print(character)
+
+print("All characters have been printed successfully.")
